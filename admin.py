24036aab98dcf0ba55252e0fdfc7b33cc90f952d
joaskvik <askvik@outlook.com>
2025-05-05 21:08:21 +0200

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,30 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
+import json
+from datetime import datetime
 
 app = Flask(__name__)
 
-BÅTLOGG_MAPPE = 'båtlogger'
+DATAFIL = 'batlogger.json'
+
+def hent_logger():
+    if not os.path.exists(DATAFIL):
+        return []
+    with open(DATAFIL, 'r', encoding='utf-8') as fil:
+        return json.load(fil)
+
+def lagre_logger(data):
+    with open(DATAFIL, 'w', encoding='utf-8') as fil:
+        json.dump(data, fil, indent=4, ensure_ascii=False)
 
 @app.route('/')
-def index():
-    båter = []
-    if os.path.exists(BÅTLOGG_MAPPE):
-        for filnavn in os.listdir(BÅTLOGG_MAPPE):
-            if filnavn.startswith('konverteringslogg_') and filnavn.endswith('.txt'):
-                båt_navn = filnavn[len('konverteringslogg_'):-len('.txt')].replace('_', ' ')
-                båter.append(båt_navn)
-    return render_template('admin_index.html', båter=båter)
+def oversikt():
+    logger = hent_logger()
+    båter = set(logg['båt'] for logg in logger)
+    return render_template('admin_oversikt.html', båter=båter)
 
-@app.route('/logg/<båtnavn>')
-def vis_båtlogg(båtnavn):
-    filsti = os.path.join(BÅTLOGG_MAPPE, f'konverteringslogg_{båtnavn.replace(" ", "_")}.txt')
-    logglinjer = []
+@app.route('/logg/<batnavn>')
+def vis_logg_for_bat(batnavn):
+    logger = hent_logger()
+    filtrert = [logg for logg in logger if logg['båt'].replace(' ', '_') == batnavn]
+    return render_template('admin_logg_bat.html', logger=filtrert, batnavn=batnavn)
 
-    if os.path.exists(filsti):
-        with open(filsti, 'r', encoding='utf-8') as f:
-            logglinjer = f.readlines()
-
-    return render_template('admin_båtlogg.html', båtnavn=båtnavn, logglinjer=logglinjer)
+@app.route('/registrer', methods=['POST'])
+def registrer():
+    data = hent_logger()
+    nytt_innslag = {
+        "båt": request.form['båt'],
+        "status": request.form['status'],
+        "tid": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    data.append(nytt_innslag)
+    lagre_logger(data)
+    return redirect(url_for('oversikt'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
